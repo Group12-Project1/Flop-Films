0f02a63aff3921ec58f232d241df046bd32f7c51
Chris <ccolon29@mail.valenciacollege.edu>
2024-05-28 21:46:22 -0400

    added the code for the tmdb api. also adjusted some values in the html form.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,6 +5,7 @@ function form() {
     var endDate = document.getElementById('end-date').value;
     var slider = document.getElementById('flop-slider').value;
 
+
     localStorage.setItem("genre", genre);
     localStorage.setItem("start-date", startDate);
     localStorage.setItem("end-date", endDate);
@@ -21,6 +22,7 @@ function form() {
 
     localStorage.setItem('ratings', JSON.stringify(ratings));
 }
+
 
 // const options = {
 //     method: 'GET',
@@ -42,9 +44,38 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjc4ODVkZGI0ZGIyNzdiZDYxZmZmMzQyYzNlZDYwNiIsInN1YiI6IjY2NTY1NjQyYzZmYTc1YjdhMDBkZWVmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjHx9Srf8U0aZGeR1372jzpSU9IJXqhjypME7388u9E'
     }
   };
+    const startDate = localStorage.getItem('start-date')
+    console.log(startDate);
+    const endDate = localStorage.getItem('end-date')
+    console.log(endDate);
+    const genre = localStorage.getItem('genre')
+    console.log(genre);
+    const sliderLarge = localStorage.getItem('slider')
+    console.log(sliderLarge);
+    const sliderSmall = sliderLarge/10
+    console.log(sliderSmall)
+    var ratings = JSON.parse(localStorage.getItem('ratings'))
+    console.log(ratings);    
+    // for (i=0; )
 
 
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=cf7885ddb4db277bd61fff342c3ed606&language=en-US&sort_by=popularity.desc&page=1&primary_release_date.lte=2004-03-11&with_genres=27&vote_average.gte=0&vote_average.lte=5', options)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=cf7885ddb4db277bd61fff342c3ed606&language=en-US&sort_by=popularity.desc&page=1&release_date.gte=${startDate}&release_date.lte=${endDate}&with_genres=${genre}&vote_average.gte=0.1&vote_average.lte=${sliderSmall}&region=US&certification=PG`, options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+
+    const genres = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjc4ODVkZGI0ZGIyNzdiZDYxZmZmMzQyYzNlZDYwNiIsInN1YiI6IjY2NTY1NjQyYzZmYTc1YjdhMDBkZWVmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjHx9Srf8U0aZGeR1372jzpSU9IJXqhjypME7388u9E'
+        }
+      };
+    
+    
+      fetch('https://api.themoviedb.org/3/certification/movie/list', genres)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+        // &certification=
